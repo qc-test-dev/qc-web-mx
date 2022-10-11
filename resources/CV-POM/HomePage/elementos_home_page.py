8fe3af550f04c8c507aa73e46ad403c553ff67c3
qc-test-dev <qc.test.dev@gmail.com>
2022-10-11 17:41:22 -0500

    Act n-charles

--- resources/CV-POM/HomePage/elementos_home_page.py
+++ resources/CV-POM/HomePage/elementos_home_page.py
@@ -1,7 +1,18 @@
+# elementos_home_page = {
+#     "home_ticker": "//div//section//button//h2[text()='Bienvenido a Claro video. Da clic aquí para conocer más planes y acceder a canales Premium.']",
+#     "home_ticker_close": "xpath=//*[@id='app']/div/section/button[2]/img",
+#     "home_logocv": "xpath=/html/body/div[2]/div/div[2]/header/div/div[1]/a/img",
+#     "home_input_buscador": "xpath=/html/body/div[2]/div/div[2]/header/div/div[2]/form/div/input",
+#     "home_lupa": "xpath=//i[@name='search']"
+# }
 elementos_home_page = {
-    "home_ticker": "//div//section//button//h2[text()='Bienvenido a Claro video. Da clic aquí para conocer más planes y acceder a canales Premium.']",
+    'home_ticker': 'xpath=//section[@class="Headband fixed visible"]//h2[1]',
     "home_ticker_close": "xpath=//*[@id='app']/div/section/button[2]/img",
-    "home_logocv": "xpath=/html/body/div[2]/div/div[2]/header/div/div[1]/a/img",
-    "home_input_buscador": "xpath=/html/body/div[2]/div/div[2]/header/div/div[2]/form/div/input",
+    "home_logocv": "xpath=//img[@alt='Clarovideo Logo']",
+    "home_input_buscador": 'xpath=//input[@placeholder="Buscar"]',
     "home_lupa": "xpath=//i[@name='search']"
+
+    #carr
+
+
 }
